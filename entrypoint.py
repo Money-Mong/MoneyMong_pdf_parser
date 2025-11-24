fa4f03ecbdece22be2b59c92733b8856226ab143
Jaehyeonee <92504386+Jaehyeonee@users.noreply.github.com>
2025-11-24 22:23:14 +0900

    test

--- entrypoint.py
+++ entrypoint.py
@@ -1,21 +1,35 @@
+print('entry 진입')
 import runpod
-from app.services.pipeline.pipeline_db_store import run_db_store_pipeline
 
 def handler(event):
-    try:
-        print("==== STARTING PDF PROCESSING ====")
-        run_db_store_pipeline()
-        print("==== COMPLETED PDF PROCESSING ====")
-        return {
-            "status": "completed",
-            "message": "PDFs parsed and stored."
-        }
-    except Exception as e:
-        print(f"ERROR in handler: {str(e)}")
-        return {
-            "status": "error",
-            "message": str(e)
-        }
+    print("=== HANDLER REACHED ===")
+    return {
+        "status": "success",
+        "input_received": event.get('input', None)
+    }
 
 if __name__ == "__main__":
     runpod.serverless.start({"handler": handler})
+
+
+# import runpod
+# from app.services.pipeline.pipeline_db_store import run_db_store_pipeline
+
+# def handler(event):
+#     try:
+#         print("==== STARTING PDF PROCESSING ====")
+#         run_db_store_pipeline()
+#         print("==== COMPLETED PDF PROCESSING ====")
+#         return {
+#             "status": "completed",
+#             "message": "PDFs parsed and stored."
+#         }
+#     except Exception as e:
+#         print(f"ERROR in handler: {str(e)}")
+#         return {
+#             "status": "error",
+#             "message": str(e)
+#         }
+
+# if __name__ == "__main__":
+#     runpod.serverless.start({"handler": handler})
